bbbdbfc33529d881acb5230170d7a942ea4f7409
Byeon Ui-Hyeok <bym4094@naver.com>
2021-06-16 15:57:31 +0900

    21.06.16 부트스트랩 적용

--- mysite/pybo/forms.py
+++ mysite/pybo/forms.py
@@ -1,17 +1,26 @@
+from pybo.models import Answer
 from django import forms
 from django.forms import widgets
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model   = Question
         fields  = ['subject', 'content']
-        widgets = {
-                    'subject' : forms.TextInput(attrs={'class':'form-control'}),
-                    'content' : forms.Textarea(attrs={'class':'form-control', 'rows' : 10})
-        } 
+        # widgets = {
+        #             'subject' : forms.TextInput(attrs={'class':'form-control'}),
+        #             'content' : forms.Textarea(attrs={'class':'form-control', 'rows' : 10})
+        # } 
         labels  = {
                     'subject' : '제목',
                     'content' : '내용'
 
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model  = Answer
+        fields = ['content']
+        label  = {
+                    'content' : '답변내용'
         }
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from .models import Question
 from django import *
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
@@ -25,11 +25,21 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     '''
-    pybo 답변 출력
+    pybo 답변 등록
     '''
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 def question_create(request):
     '''
@@ -46,4 +56,3 @@ def question_create(request):
         form = QuestionForm()
     context = { 'form' : form }
     return render(request, 'pybo/question_form.html', context)
-
