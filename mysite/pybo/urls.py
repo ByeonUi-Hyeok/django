3cd5b43864612652a0bcbfa977b379c12b227944
Byeon Ui-Hyeok <bym4094@naver.com>
2021-06-18 11:44:55 +0900

    21.06.18 수정기능 추가

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
                     # path/로 오면 view.index로 처리한다는 것
                     path('<int:question_id>/', views.detail, name='detail'),
                     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
-                    path('question/create/', views.question_create, name='question_create')
+                    path('question/create/', views.question_create, name='question_create'),
+                    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
 ]
