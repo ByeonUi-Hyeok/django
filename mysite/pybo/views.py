c053a28961b2c087350bc081a1a91452d0861e7d
Byeon Ui-Hyeok <bym4094@naver.com>
2021-06-18 13:52:58 +0900

    21.06.18 댓글 수정 및 삭제 기능 추가

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -2,7 +2,7 @@
 from django.core import paginator
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Question
+from .models import *
 from django import *
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
@@ -109,3 +109,38 @@ def question_delete(request, question_id):
         return redirect('pybo:detail', question_id=question.id)
     question.delete()
     return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """
+    pybo 답변수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    """
+    pybo 답변삭제
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
